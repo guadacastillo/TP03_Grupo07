f6c221838c857e3dce5625a66ad8bae668c2a7d1
guadacastillo <135341339+guadacastillo@users.noreply.github.com>
2023-06-02 11:01:40 -0300

    Update ProductoController.java

--- Tp03-Grupo07/src/main/java/ar/edu/unju/edm/controller/ProductoController.java
+++ Tp03-Grupo07/src/main/java/ar/edu/unju/edm/controller/ProductoController.java
@@ -5,11 +5,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 import ar.edu.unju.edm.model.Producto;
 import ar.edu.unju.edm.service.ProductoService;
 import ar.edu.unju.edm.util.Productos;
+import java.util.Optional;
 
 @Controller
 
@@ -28,13 +30,14 @@ public class ProductoController {
 		
 	}
 
+	/*
 	@GetMapping("/cargarProducto")
-	
 	public ModelAndView cargarProductos() {
 		ModelAndView nuevo= new ModelAndView ("formulario"); 
 		nuevo.addObject("producto", unProducto); 
 		return nuevo;
 	}
+	*/
 	
 	@PostMapping("/guardarProducto")
 	public ModelAndView guardarProducto(@ModelAttribute("formulario") Producto productoConDatos) {
@@ -43,6 +46,16 @@ public class ProductoController {
 		nuevo.addObject("listado", productoService.listarTodosProductos()); 
 		
 		return nuevo;
+	}
+	
+	@GetMapping("/cargarProducto")
+    	public ModelAndView mostrarFormulario(@RequestParam("codigo") Optional<Integer> codigo) {
+        	Optional<Producto> optProducto = codigo.map(productosService::recuperarProducto).orElse(Optional.of(new Producto()));
+		Producto producto = optProducto.get().orElse(null);
+		ModelAndView nuevo = new ModelAndView("formulario");
+		nuevo.addObject("producto", producto);
+
+        	return nuevo;
 	}
 	
 	
